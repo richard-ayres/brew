50541513206a0a5b69a54e43d6d1f92773311295
Richard Ayres <richard@bitspear.co.uk>
2018-09-24 09:46:22 +0100

    accurate ABV calculator now "uses" ABW calculator

--- calculators/accurate_abv_calculator.py
+++ calculators/accurate_abv_calculator.py
@@ -1,9 +1,19 @@
-from .abwcalculator import ABWCalculator
+from .calculator import Calculator
+from .abw_calculator import ABWCalculator
 
-class AccurateABVCalculator(ABWCalculator):
+class AccurateABVCalculator(Calculator):
+    required = {'original-gravity', 'final-gravity'}
+    defaults = {'temperature': 20, 'calibration-temperature': 20}
 
     def calculate(self):
-        abw = super().calculate()
 
-        return abw * self['final-gravity'] / 0.7907
+        abw_calculator = ABWCalculator({
+            'final-gravity': self['final-gravity'],
+            'original-gravity': self['original-gravity'],
+            'temperature': self['temperature'],
+            'calibration-temperature': self['calibration-temperature']})
+
+        abw = abw_calculator.calculate()
+
+        return abw * float(self['final-gravity']) / 0.7907
 
